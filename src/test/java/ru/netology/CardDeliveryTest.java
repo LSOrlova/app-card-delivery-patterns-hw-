2844a97f7d05017fd66be1cc4042a783d85c067b
Neto test <lsorlova82@gmail.com>
2023-03-20 14:06:50 +0200

    first task is successful

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,54 +1,63 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterEach;
+import com.github.javafaker.CreditCardType;
+import com.github.javafaker.Faker;
+import lombok.experimental.var;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
+import ru.netology.data.UserInfo;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CardDeliveryTest {
-
-
-    String meetingDay(int day) {
-        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-    String secondMeetingDay(int day) {
-        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-
     @BeforeEach
     void setUp() {
         open("http://localhost:9999/");
     }
 
     @Test
-    void testHappyPath() {
+    void testHappyPathOrderMeeting() {
+        int daysToAddFirstMeeting = 3;
+        String firstMeetingDate = DataGenerator.generateDate(daysToAddFirstMeeting);
         SelenideElement form = $("[action='/']");
-        form.$("[data-test-id='city'] input").setValue("Нижний Новгород");
-        form.$("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        form.$("[data-test-id='date'] input").setValue(meetingDay(3));
-        form.$("[data-test-id='name'] input").setValue("Денис Кюстина");
-        form.$("[data-test-id='phone'] input").setValue("+72743275118");
+        form.$("[data-test-id='city'] input").val(DataGenerator.generateCity());
+        form.$("[data-test-id='date'] input").sendKeys(firstMeetingDate);
+        form.$("[data-test-id='name'] input").val(DataGenerator.generateName());
+        form.$("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
         form.$("[data-test-id='agreement']").click();
         form.$(".button__content").click();
         $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на "
-                + meetingDay(3)));
+                + firstMeetingDate));
+    }
+    @Test
+    void testHappyPathReOrderMeeting() {
+        int daysToAddFirstMeeting = 3;
+        int daysToAddSecondMeeting = 3;
+        String firstMeetingDate = DataGenerator.generateDate(daysToAddFirstMeeting);
+        String secondMeetingDate = DataGenerator.generateDate(daysToAddSecondMeeting);
+        SelenideElement form = $("[action='/']");
+        form.$("[data-test-id='city'] input").val(DataGenerator.generateCity());
+        form.$("[data-test-id='date'] input").sendKeys(firstMeetingDate);
+        form.$("[data-test-id='name'] input").val(DataGenerator.generateName());
+        form.$("[data-test-id='phone'] input").val(DataGenerator.generatePhone());
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button__content").click();
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на "
+                + firstMeetingDate));
         $(".button").click();
         $("[data-test-id='replan-notification']").shouldHave(text("У вас уже запланирована встреча на другую дату." +
                 " Перепланировать?"));
         $(byText("Перепланировать")).click();
-        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на "
-                + secondMeetingDay(3))); // здесь должен быть другой день?
+        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
